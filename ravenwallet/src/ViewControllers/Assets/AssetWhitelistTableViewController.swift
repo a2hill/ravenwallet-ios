baaf2500fb5f44e2f397e178ee1353a84c92d010
Austin Hill <austin@sparkaerial.com>
2020-05-18 18:06:57 -0700

    Tested with single asset

--- ravenwallet/src/ViewControllers/Assets/AssetWhitelistTableViewController.swift
+++ ravenwallet/src/ViewControllers/Assets/AssetWhitelistTableViewController.swift
@@ -21,6 +21,10 @@ class AssetWhitelistTableViewController: UITableViewController {
                 return nil
             }
         }
+        
+        var footerHeight: CGFloat {
+            return 40
+        }
     }
     
     private let emptyMessage = UILabel.wrapping(font: .customBody(size: 16.0), color: .grayTextTint)
@@ -42,7 +46,7 @@ class AssetWhitelistTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.register(AssetManageCell.self, forCellReuseIdentifier: AssetManageCell.cellIdentifier)
+        tableView.register(WhitelistTableViewCell.self, forCellReuseIdentifier: WhitelistTableViewCell.reuseIdentifier)
 
         tableView.separatorStyle = .none
         tableView.estimatedRowHeight = 60.0
@@ -53,32 +57,47 @@ class AssetWhitelistTableViewController: UITableViewController {
         emptyMessage.textAlignment = .center
         emptyMessage.text = S.Asset.emptyMessage
         
+        tableView.setEditing(false, animated: false)
+        
         setContentInset()
     }
+}
+
+// MARK: - Styling
+extension AssetWhitelistTableViewController {
     
     private func setContentInset() {
         let insets = UIEdgeInsets(top: manageAssetHeaderHeight - 64.0 - (E.isIPhoneXOrLater ? 28.0 : 0.0), left: 0, bottom: C.padding[2], right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: WhitelistTableViewCell.reuseIdentifier, for: indexPath) as! WhitelistTableViewCell
-        
-        guard let section = Section(for: indexPath) else { return cell }
-        
-        let assetName: String
+    
+    private func getFooter(for section: Section) -> UIView? {
+        let footerText: String
         
         switch section {
         case .whitelist:
-            assetName = whitelistNames[indexPath.row]
-            
+            guard whitelistNames.count == 0 else { return nil }
+            footerText = "TODO"
         case .nonWhitelist:
-            assetName = nonWhitelistNames[indexPath.row]
+            guard nonWhitelistNames.count == 0 else { return nil }
+            footerText = S.Asset.emptyMessage
         }
         
-        cell.assetName = assetName
-        return cell
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: section.footerHeight))
+        footerView.backgroundColor = UIColor.white
+        
+        let emptyMessage = UILabel.wrapping(font: .customBody(size: 16.0), color: .grayTextTint)
+        footerView.addSubview(emptyMessage)
+        emptyMessage.text = footerText
+        emptyMessage.textAlignment = .center
+        
+        emptyMessage.constrain([
+            emptyMessage.centerXAnchor.constraint(equalTo: footerView.centerXAnchor),
+            emptyMessage.centerYAnchor.constraint(equalTo: footerView.centerYAnchor),
+            emptyMessage.widthAnchor.constraint(equalTo: footerView.widthAnchor, constant: -C.padding[2]) ])
+        
+        return footerView
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -86,62 +105,27 @@ class AssetWhitelistTableViewController: UITableViewController {
         
         switch section {
         case .whitelist:
-            return "Whitelisted Assets"
+            return "TODO: Whitelisted Assets"
             
         case .nonWhitelist:
-            return "Available Assets"
+            return "TODO: Available Assets"
         }
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        guard let section = Section(rawValue: section) else { return nil }
+        
+        return getFooter(for: section)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        guard let section = Section(rawValue: section) else { return 0 }
+        
+        return getFooter(for: section)?.bounds.height ?? 0.0
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
-// MARK: - Table view data source
+// MARK: - Data source
 extension AssetWhitelistTableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -161,26 +145,69 @@ extension AssetWhitelistTableViewController {
             return nonWhitelistNames.count
         }
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: WhitelistTableViewCell.reuseIdentifier, for: indexPath) as! WhitelistTableViewCell
+        
+        guard let section = Section(for: indexPath) else { return cell }
+        
+        let assetName: String
+        
+        switch section {
+        case .whitelist:
+            assetName = whitelistNames[indexPath.row]
+            
+        case .nonWhitelist:
+            assetName = nonWhitelistNames[indexPath.row]
+        }
+        
+        cell.assetName = assetName
+        return cell
+    }
 }
 
-// MARK: - Table view interaction handler
+// MARK: - Interaction
 extension AssetWhitelistTableViewController {
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let section = Section(for: indexPath) else { return }
         
         switch section {
         case .whitelist:
-            tableView.beginUpdates()
+
             let assetName = whitelistNames[indexPath.row]
             assetManager.removeFromWhitelist(assetName: assetName)
-            tableView.endUpdates()
+            tableView.reloadData()
             
         case .nonWhitelist:
-            tableView.beginUpdates()
             let assetName = nonWhitelistNames[indexPath.row]
             assetManager.addToWhitelist(assetName: assetName)
-            tableView.endUpdates()
+            tableView.reloadData()
+        }
+    }
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        guard let section = Section(for: indexPath) else { return false}
+        switch section {
+        case .whitelist: return true
+        case .nonWhitelist: return false
+        }
+    }
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard let section = Section(for: indexPath) else { return }
+        
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            if section == .whitelist {
+                assetManager.whitelist.remove(whitelistNames[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
     }
 }
--- ravenwallet/src/ViewControllers/Assets/WhitelistTableViewCell.swift
+++ ravenwallet/src/ViewControllers/Assets/WhitelistTableViewCell.swift
@@ -22,11 +22,4 @@ class WhitelistTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
-
 }
